9897ee6b311be0c35415c0a2d5198322f1fd6ffa
LucasGomezz <lucasgvilla@hotmail.com>
2024-06-04 17:08:05 -0300

    Hicimos el Entity Framework

---
+++ BotPeliculas.Bot/Controllers/UsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using BotPelicualas.Logica;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotPeliculas.Bot.Controllers;
+
+public class UsuarioController : Controller
+{
+    private static UsuarioServicio _usuarioServicio;
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
